7d9f201c48a5b67eaf0077675fbb4c91f40cb359
sarahschaeferr <143412329+sarahschaeferr@users.noreply.github.com>
2023-09-21 16:54:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function collegeAnswer() {
+  var password = document.getElementById("quizquestion");
+  var passwordText = password.value.toUpperCase();
+  if(passwordText == "THE UNIVERSITY OF TAMPA") {
+  return true;
+}
+  alert("That is incorrect, try again!");
+  return false;
+}
